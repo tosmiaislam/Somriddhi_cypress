a8771df75b9d25c927f503f2e3d6ffa179647e9b
rijviahmedrahat <angelnishat4746@gmail.com>
2024-07-11 16:37:37 +0600

    new change

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,8 +7,11 @@ describe('template spec', () => {
       cy.get("input[placeholder='01XXXXXXXXX']").type(data.email); 
       cy.get("input[placeholder='Enter password']").type(data.password);
       cy.get('.login-form > .text-center > .btn').click();
-      cy.get('.menu-is-opening > [href="#"] > p > .right').click();
-     
+      cy.xpath("//p[normalize-space()='Settings']").should('be.visible').click()  
+      cy.xpath("//p[normalize-space()='Commissions']").should('be.visible').click()  
+      
+    //a[@class='nav-link active']
+   
     //   cy.get('.HomePage-service > .MuiTypography-root').click().wait(3000);
     //   cy.xpath("//div[@class='AppHeader-sectionDesktop MuiBox-root css-0']//i[@class='flag flag-24 flag-bd']").click();
     //   cy.contains('a[title="User Management"]', 'User Management').find('span').wait(3000).click();
